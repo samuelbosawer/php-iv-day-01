12a8ba9692b78fc4e74f9ec0a5ceaec828408297
Samuel Bosawer <zam.bosawer@gmail.com>
2023-06-16 11:54:33 +0900

    ✅🍻 fix

---
+++ home.php
@@ -0,0 +1,12 @@
+    <?php
+        include_once('include/header.php');
+        $title = 'HOME';
+
+    ?>
+    
+    <h1> <?= $title?>  </h1>
+
+    <a href="index.php">Kembali</a>
+
+
+    <?php include_once('include/footer.php') ?>
--- index.php
+++ index.php
@@ -1,6 +1,17 @@
-<?php 
-    phpinfo();
-    $txt = "PHP BANGET";
-    echo "I LOVE " . $txt;
-    echo "<p> Hello Word </p>"
-?>
+    <?php
+        include_once('include/header.php');
+        // Nama Lengkap
+        $namaDepan = 'Samuel';
+        $namaBelakang = 'Bosawer';
+
+        $textWelcome = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. In, suscipit!';
+        $textSubTitle = 'Belajar PHP DAY 01';
+    ?>
+
+    <!-- Hello -->
+    <h1>Hello  <?php echo $namaDepan .' '.$namaBelakang?> </h1>
+    <h2> <?= $textSubTitle?> </h2>
+    <p> <?= $textWelcome?> </p>
+    <a href="home.php"> Halaman Home</a>
+
+    <?php include_once('include/footer.php') ?>
